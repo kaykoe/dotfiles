8841e55cf6b732b6061206b42bfabf5e6a444c43
kaykoe <karolkoenigg@gmail.com>
2025-09-22 11:52:10 +0200

    python debugger config for nvim

---
+++ private_dot_config/nvim/lua/plugins/nvim-dap-python.lua
@@ -0,0 +1,15 @@
+return {
+	"mfussenegger/nvim-dap-python",
+	-- stylua: ignore
+	config = function()
+		if vim.fn.has("win32") == 1 then
+			require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/Scripts/pythonw.exe"))
+		else
+			require("dap-python").setup("uv")
+		end
+		for _, config in ipairs(require("dap").configurations.python) do
+			config.justMyCode = false
+		end
+	end
+,
+}
