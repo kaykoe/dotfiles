70fad0a184c2cc9f0142c192f9b5d69ec2712426
kaykoe <karolkoenigg@gmail.com>
2025-09-23 12:14:54 +0200

    reformat

--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -1,33 +1,33 @@
 return {
-    "saghen/blink.cmp",
-    opts = {
-        keymap = {
-            preset = "default",
-        },
-        signature = {
-            enabled = true,
-            window = {
-                show_documentation = false,
-            },
-        },
-        completion = {
-            documentation = {
-                auto_show = false,
-            },
-        },
-        cmdline = {
-            enabled = true,
-            keymap = {
-                ["<Tab>"] = { "show", "accept" },
-            },
-            completion = {
-                menu = {
-                    ---@diagnostic disable-next-line: unused-local
-                    auto_show = function(ctx)
-                        return vim.fn.getcmdtype() == ":"
-                    end,
-                },
-            },
-        },
-    },
+	"saghen/blink.cmp",
+	opts = {
+		keymap = {
+			preset = "default",
+		},
+		signature = {
+			enabled = true,
+			window = {
+				show_documentation = false,
+			},
+		},
+		completion = {
+			documentation = {
+				auto_show = false,
+			},
+		},
+		cmdline = {
+			enabled = true,
+			keymap = {
+				["<Tab>"] = { "show", "accept" },
+			},
+			completion = {
+				menu = {
+					---@diagnostic disable-next-line: unused-local
+					auto_show = function(ctx)
+						return vim.fn.getcmdtype() == ":"
+					end,
+				},
+			},
+		},
+	},
 }
