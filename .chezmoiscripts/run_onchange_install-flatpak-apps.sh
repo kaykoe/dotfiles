90640da738c0263d78e454944c64d8880cd399ab
kaykoe <karolkoenigg@gmail.com>
2026-02-07 21:41:14 +0100

    fix: add flathub to flatpak remotes, make sure the script fails

--- .chezmoiscripts/run_onchange_install-flatpak-apps.sh
+++ .chezmoiscripts/run_onchange_install-flatpak-apps.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+set -e
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub md.obsidian.Obsidian dev.vencord.Vesktop
 sudo flatpak override --socket=wayland --socket=fallback-x11 md.obsidian.Obsidian
