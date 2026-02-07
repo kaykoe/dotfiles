c4dd5361c3bcbb53cecc9a974b0c47a8e35a38fa
kaykoe <karolkoenigg@gmail.com>
2026-02-07 16:49:24 +0100

    fix: kanata new setup to work with newer systemd

--- .chezmoiscripts/run_once_before_20-setup-kanata.sh
+++ .chezmoiscripts/run_once_before_20-setup-kanata.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo 'Setting up Kanata'
-sudo groupadd uinput
-echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee -a /etc/udev/rules.d/99-input.rules >/dev/null
+sudo groupadd --system uinput
+echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee /etc/udev/rules.d/99-input.rules >/dev/null
 sudo udevadm control --reload-rules && sudo udevadm trigger
 sudo modprobe uinput
 sudo useradd --system --no-create-home -s /sbin/nologin -G uinput,input kanata
