4a94725f7efe077e57dc6402583f1885ace7e480
kaykoe <karolkoenigg@gmail.com>
2026-08-10 13:52:15 +0200

    fix: crush config

--- private_dot_config/crush/approve-readonly-shell.sh
+++ private_dot_config/crush/approve-readonly-shell.sh
@@ -6,7 +6,7 @@ case "$command" in
 *';'* | *'|'* | *'&'* | *'`'* | *'$'* | *'('* | *')'* | *'>'* | *'<'* | *$'\n'*)
 	exit 0
 	;;
-chezmoi\ execute-template\ * | shellcheck\ \* | *\ --help | pwd | ls | ls\ * | tree | tree\ * | which\ * | command\ -v\ * | type\ * | test\ * | printf\ * | print\ * | echo\ * | date | date\ * | basename\ * | dirname\ * | realpath\ * | readlink\ * | cat\ * | head\ * | tail\ * | grep\ * | rg\ * | find\ * | file\ * | stat\ * | wc\ * | diff\ * | sed\ -n\ * | bash\ -n\ * | zsh\ -n\ * | npm\ list | npm\ list\ * | npm\ info\ * | pdftotext\ * | notify-send\ * | kubectl\ auth\ can-i\ *)
+chezmoi\ diff | chezmoi\ diff\ * | chezmoi\ execute-template\ * | shellcheck\ \* | *\ --help | pwd | ls | ls\ * | tree | tree\ * | which\ * | command\ -v\ * | type\ * | test\ * | printf\ * | print\ * | echo\ * | date | date\ * | basename\ * | dirname\ * | realpath\ * | readlink\ * | cat\ * | head\ * | tail\ * | grep\ * | rg\ * | find\ * | file\ * | stat\ * | wc\ * | diff\ * | sed\ -n\ * | bash\ -n\ * | zsh\ -n\ * | npm\ list | npm\ list\ * | npm\ info\ * | pdftotext\ * | notify-send\ * | kubectl\ auth\ can-i\ *)
 	printf '%s\n' '{"decision":"allow"}'
 	;;
 esac
