e3c7ad2ab891da88aa325e9228914ef5424f85f0
kaykoe <karolkoenigg@gmail.com>
2026-09-02 13:28:42 +0200

    fix: auto set zen as default browser after installation

--- .chezmoiscripts/run_once_install-zen.sh
+++ .chezmoiscripts/run_once_install-zen.sh
@@ -2,3 +2,5 @@
 
 set -eo pipefail
 curl -fsSL https://updates.zen-browser.app/install.sh | bash
+update-alternatives --set x-www-browser ~/.local/bin/zen
+xdg-settings set default-web-browser microsoft-edge.desktop
